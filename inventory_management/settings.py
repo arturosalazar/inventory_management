560f8af8b292e7f1ad1bcfaedbb3025358fe11f7
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-01-08 19:33:36 -0600

    Add crispy form to project and update form styling

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -41,7 +41,13 @@ INSTALLED_APPS = [
     # User Created Apps
     'inventory',
     # Third Party Apps
+    'crispy_forms',
+    "crispy_bootstrap5",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
