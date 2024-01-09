4a8d998ab0369cc2c340a248baad338011cebc2d
Arturo Salazar <arturo.v.salazar.jr@gmail.com>
2024-01-08 19:30:14 -0600

    Add signup page

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Index
+from .views import Index, SignUp
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
+    path('signup/', SignUp.as_view(), name='signup'),
 ]
